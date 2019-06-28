bae5dec72451293dba86430e348497b38ddb9aef
startnoob <1694487365@qq.com>
2019-06-28 18:32:11 +0800

    update

---
+++ CodeForces/1186/a/ac.cpp
@@ -0,0 +1,33 @@
+/*************************************************************************
+	> File Name: ac.cpp
+	> Author: ralph
+	> Mail: 1694487365@qq.com
+	
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdio>
+#include<fstream>
+#include<algorithm>
+#include<cmath>
+#include<deque>
+#include<vector>
+#include<queue>
+#include<string>
+#include<cstring>
+#include<map>
+#include<stack>
+#include<set>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pi 3.14159265
+#define e 2.71828182
+#define fo(x1,x2,x3,x4) for(int x1=x2;x1<=x3;x1+=x4)
+int main()
+{
+    int n,m,k;
+    cin>>n>>m>>k;
+    if(n<=min(m,k)) cout<<"Yes";
+    else cout<<"No";
+}

474b874264c1913a07b29a53421b4e3d3cded49e
startnoob <1694487365@qq.com>
2019-08-16 07:55:15 +0800

    update matches

--- CodeForces/16/b/ac.cpp
+++ CodeForces/16/b/ac.cpp
@@ -1,5 +1,5 @@
 /*************************************************************************
-	> File Name: zhong.cpp
+	> File Name: ac.cpp
 	> Author: ralph
 	> Mail: 1694487365@qq.com
 	
@@ -24,10 +24,29 @@ using namespace std;
 #define pi 3.14159265
 #define e 2.71828182
 #define fo(x1,x2,x3,x4) for(int x1=x2;x1<=x3;x1+=x4)
-int arr[1000];
+struct node
+{
+    int box,matches;
+}en[30];
+bool cmp(node x,node y)
+{
+    if(x.matches==y.matches) return x.box>y.box;
+    return x.matches>y.matches;
+}
 int main()
 {
-    int n;
-    cin>>n;
-    cout<<n%10;
+    int n,m;
+    scanf("%d%d",&n,&m);
+    for(int i=0;i<m;i++) scanf("%d%d",&en[i].box,&en[i].matches);
+    sort(en,en+m,cmp);
+    //for(int i=0;i<m;i++) cout<<en[i].box<<" "<<en[i].matches<<endl;
+    int tail=0;
+    long long cnt=0;
+    while(tail<m&&n>en[tail].box)
+    {
+        cnt+=en[tail].box*en[tail].matches;
+        n-=en[tail++].box;
+    }
+    cnt+=n*en[tail].matches;
+    printf("%lld",cnt);
 }
